3f2e4c7888a761ffe43f8410b3d48206df164487
Dmitry Sichkar <dmmeteo@gmail.com>
2018-07-17 15:20:25 +0300

    global and local nesting components

---
+++ src/components/Ninjas.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="ninja in ninjas">{{ninja}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      ninjas: ['Yoshi', 'Mario', 'Ryu']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
